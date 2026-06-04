ba5aa5e05bc824e0431930612fd22dcd44f7dca7
gardenbylit <45648174+gardenbylit@users.noreply.github.com>
2026-06-04 18:09:58 +0700

    Add ScannerScreen component

---
+++ src/screens/ScannerScreen.js
@@ -0,0 +1,14 @@
+import React, { useRef, useState } from 'react';
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  Alert,
+  ActivityIndicator,
+  Modal,
+  ScrollView,
+  SafeAreaView,
+} from 'react-native';
+import { RNCamera } from 'react-native-camera';
+import { Ionicons } from '@expo/vector-icons';
